5b5f6c676bfdb76fa6ae3864889435a3c12d0c06
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-25 21:51:00 +0100

    Feat --> Adding request body of store method

--- src/interface/http/eventController.ts
+++ src/interface/http/eventController.ts
@@ -1,40 +1,61 @@
 import { Request, Response } from "express";
 import {EventUseCase} from "../../application/usecases/eventUseCase";
+import {EventDTO} from "../../core/dto/EventDTO";
+import {EventEntity} from "../../core/entities/eventEntity";
 
 export class EventController{
     private readonly eventUseCase : EventUseCase
 
     constructor(eventUseCase : EventUseCase) {this.eventUseCase = eventUseCase;}
 
-    async store(req : Request, res : Response): Promise<Response>{
+    async store(req: Request, res: Response): Promise<Response> {
         try {
+            const { title, description , participants} = req.body;
+            const image = req.file?.filename;
 
-        }catch (e) {
-            return res.status(500).json({ success: false, message: e })
+            if (!title || !description || !image) {
+                return res.status(400).json({ success: false, message: "All fields are required, including the image" });
+            }
+
+            const eventDto: EventDTO = new EventDTO(title, image, description, participants);
+
+            const event: EventEntity = await this.eventUseCase.store(eventDto);
+            return res.status(201).json({ success: true, data: event });
+
+        } catch (e) {
+            return res.status(500).json({ success: false, message: e || "An error occurred" });
         }
     }
 
-    async update(req : Request, res : Response): Promise<Response>{
-        try {
+    // async update(req : Request, res : Response): Promise<Response>{
+    //     try {
+    //         const {id}: string = req.params
+    //         const {title, image, description, participant} = req.body
+    //         if (!title || !image || !description || !participant) {
+    //             return res.status(400).json({ success: false, message: "fields is required" })
+    //         }
+    //         const eventDto: EventDTO = new EventDTO(title, image, description, participant)
+    //         const event: EventEntity = await this.eventUseCase.update(id, eventDto)
+    //         return res.status(201).json({success: true, data: event})
+    //     }catch (e) {
+    //         res.status(500).json({ success: false, message: e })
+    //     }
+    // }
 
+    async index(req : Request, res : Response){
+        try {
+            const event = await this.eventUseCase.index()
+            return res.status(200).json(event)
         }catch (e) {
             res.status(500).json({ success: false, message: e })
         }
     }
 
-    async index(req : Request, res : Response): Promise<Response>{
-        try {
-
-        }catch (e) {
-            res.status(500).json({ success: false, message: e })
-        }
-    }
-
-    async delete(req : Request, res : Response): Promise<Response>{
-        try {
-
-        }catch (e) {
-            res.status(500).json({ success: false, message: e })
-        }
-    }
+    // async delete(req : Request, res : Response){
+    //     try {
+    //
+    //     }catch (e) {
+    //         res.status(500).json({ success: false, message: e })
+    //     }
+    // }
 }
