0e08ddb640179c9bcc9f00754e168743ec6d186a
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-25 22:34:39 +0100

    Feat --> Adding Field of organiser in eventEntity

--- src/core/entities/eventEntity.ts
+++ src/core/entities/eventEntity.ts
@@ -1,10 +1,11 @@
-import { Types } from "mongoose";
+import mongoose from "mongoose";
 
 export class EventEntity {
     constructor(
         public readonly title: string,
         public readonly image: string,
         public readonly description: string,
-        public readonly participants: any
+        public readonly participants: any,
+        public readonly organiser: any
     ) {}
 }
