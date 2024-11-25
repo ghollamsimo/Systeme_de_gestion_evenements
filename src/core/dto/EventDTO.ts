42e3bea6818907e0364d337f3b7f213082795080
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-25 22:34:54 +0100

    Feat --> Adding Field of organiser in eventDTO

--- src/core/dto/EventDTO.ts
+++ src/core/dto/EventDTO.ts
@@ -1,20 +1,22 @@
-import { Types } from "mongoose";
+import mongoose from "mongoose";
 
 export class EventDTO {
     readonly title: string;
     readonly image: string;
     readonly description: string;
     readonly participants: any
-
+    readonly organiser: any
     constructor(
         title: string,
         image: string,
         description: string,
-        participants: any
+        participants: any,
+        organiser: any
     ) {
         this.title = title;
         this.image = image;
         this.description = description;
         this.participants = participants;
+        this.organiser = organiser
     }
 }
