392d165895f749ffdaf8d34e82d85eecce621519
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-26 10:11:50 +0100

    Feat --> Adding testing of method store and impoting jest.mock

--- __test__/organiser.test.ts
+++ __test__/organiser.test.ts
@@ -1,5 +1,8 @@
 import {OrganiserUseCase} from "../src/application/usecases/organiserUseCase";
 import {OrganiserRepositoryImpl} from "../src/infrastructure/repositories/organiserRepositoryImpl";
+import {UserDto} from "../src/core/dto/UserDto";
+
+jest.mock("../src/application/usecases/organiserUseCase");
 
 describe(' Testing Crud of Participant', () => {
     let organiserUseCase: OrganiserUseCase
@@ -8,4 +11,16 @@ describe(' Testing Crud of Participant', () => {
         organiserUseCase = new OrganiserUseCase(organiserRepositoryImpl)
     })
 
+    it('should call method of storing', () => {
+        const storeMethod = jest.spyOn(organiserUseCase , 'store')
+        const userDto: UserDto = {
+            name: 'test',
+            email: 'test@example.com',
+            password: 'test',
+            role: 'participant'
+        }
+        organiserUseCase.store(userDto)
+        expect(storeMethod).toBeCalledTimes(1)
+        expect(storeMethod).toBeCalledWith(userDto)
+    })
 })
