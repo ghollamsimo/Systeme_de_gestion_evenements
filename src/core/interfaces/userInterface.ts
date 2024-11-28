d1d654fd58dca4a24529047e7409116424e07451
ghollamsimo <ghollamsimo1@gmail.com>
2024-11-28 09:33:28 +0100

    Feat --> Adding method of show and take as argumant an id as string

--- src/core/interfaces/userInterface.ts
+++ src/core/interfaces/userInterface.ts
@@ -4,7 +4,7 @@ import {LoginDTO} from "../dto/LoginDTO";
 export interface UserInterface {
     register(user: UserEntity): Promise<UserEntity>
     login(loginDto:LoginDTO): Promise<{token: string}>
-
+    show(id: string)
     forgotPassword(email: string): Promise<void>
     resetPassword(resetToken: string, newPassword: string): Promise<{ message: string }>
 }
